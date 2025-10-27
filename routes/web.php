9842818c9a80fcb9c07c39f31535cb7be33e1646
felipesantos2 <santospinheiro627@gmail.com>
2025-10-27 10:36:47 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    try {
+        dd(DB::connection()->getPdo());
+    } catch (Exception $e) {
+        dd($e->getMessage());
+    }
 });
--- todo.md
+++ todo.md
@@ -1,3 +1,6 @@
-(A) dockerize app  
-(B) 
-(C) 
+[x] add pint settings
+[] check session drivers
+[] check queue drivers
+[] add alias for windows (pint, reator, ...)
+[] dockerize application
+[] add alias for linux
